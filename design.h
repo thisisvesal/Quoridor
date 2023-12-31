4c11f431c5a89ebee2eb5eed22693593a5cc779c
thisisvesal <vesal.ea84@gmail.com>
2023-12-31 13:43:35 +0330

    considered invalid inputs

--- design.h
+++ design.h
@@ -1,10 +1,10 @@
-# include <stdio.h>
-# include <windows.h>
-# include "boardMaker.h"
-# include "boardPrinter.h"
+#include <stdio.h>
+#include <windows.h>
+#include "boardMaker.h"
+#include "boardPrinter.h"
 
-# ifndef design
-# define design
+#ifndef design
+#define design
 
 // for clearing screen
 void clearScreen()
@@ -12,7 +12,7 @@ void clearScreen()
     system("cls");
 }
 
-//changing color
+// changing color
 void setTextColor(int textColor, int backColor)
 {
     HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -25,22 +25,15 @@ void arrowKeyPrint()
 {
     char keyArr[5][7];
 
-    keyArr[0][0] = keyArr[0][1] = keyArr[0][5] = keyArr[0][6]
-            = keyArr[1][0] = keyArr[1][1] = keyArr[1][5] = keyArr[1][6]
-            = ' ';
+    keyArr[0][0] = keyArr[0][1] = keyArr[0][5] = keyArr[0][6] = keyArr[1][0] = keyArr[1][1] = keyArr[1][5] = keyArr[1][6] = ' ';
 
     keyArr[0][2] = keyArr[2][0] = 218;
 
     keyArr[0][4] = keyArr[2][6] = 191;
 
-    keyArr[0][3]
-            = keyArr[2][1] = keyArr[2][3] = keyArr[2][5]
-            = keyArr[4][1] = keyArr[4][3] = keyArr[4][5]
-            = 196;
+    keyArr[0][3] = keyArr[2][1] = keyArr[2][3] = keyArr[2][5] = keyArr[4][1] = keyArr[4][3] = keyArr[4][5] = 196;
 
-    keyArr[1][2] = keyArr[1][4]
-            = keyArr[3][0] = keyArr[3][2] = keyArr[3][4] = keyArr[3][6]
-            = 179;
+    keyArr[1][2] = keyArr[1][4] = keyArr[3][0] = keyArr[3][2] = keyArr[3][4] = keyArr[3][6] = 179;
 
     keyArr[2][2] = keyArr[2][4] = 197;
 
@@ -63,7 +56,6 @@ void arrowKeyPrint()
         }
         printf("\n ");
     }
-
 }
 
 // choosing a theme
@@ -71,75 +63,89 @@ int chooseBoard()
 {
     int num;
 
-    setTextColor(0 , 15);
+    setTextColor(0, 15);
     printf("Choose your board:\n");
     makeBoard(2, 2);
 
     // black board
-    setTextColor(0 , 15);
+    setTextColor(0, 15);
     printf("1) \n");
-    setTextColor(0 , 15);
-    printBoard(Board,2,2);
+    setTextColor(0, 15);
+    printBoard(Board, 2, 2);
     printf("\n");
 
-    //gold board
-    setTextColor(0 , 15);
+    // gold board
+    setTextColor(0, 15);
     printf("2) \n");
-    setTextColor(6 , 15);
-    printBoard(Board,2,2);
+    setTextColor(6, 15);
+    printBoard(Board, 2, 2);
     printf("\n");
 
-    //purple board
-    setTextColor(0 , 15);
+    // purple board
+    setTextColor(0, 15);
     printf("3) \n");
-    setTextColor(13 , 15);
-    printBoard(Board,2,2);
+    setTextColor(13, 15);
+    printBoard(Board, 2, 2);
     printf("\n");
 
-    //red board
-    setTextColor(0 , 15);
+    // red board
+    setTextColor(0, 15);
     printf("4) \n");
-    setTextColor(12 , 15);
-    printBoard(Board,2,2);
+    setTextColor(12, 15);
+    printBoard(Board, 2, 2);
     printf("\n");
 
-    //light blue board
-    setTextColor(0 , 15);
+    // light blue board
+    setTextColor(0, 15);
     printf("5) \n");
-    setTextColor(11 , 15);
-    printBoard(Board,2,2);
+    setTextColor(11, 15);
+    printBoard(Board, 2, 2);
     printf("\n");
 
-    //green board
-    setTextColor(0 , 15);
+    // green board
+    setTextColor(0, 15);
     printf("6) \n");
-    setTextColor(10 , 15);
-    printBoard(Board,2,2);
+    setTextColor(10, 15);
+    printBoard(Board, 2, 2);
     printf("\n");
 
-    //dark blue board
-    setTextColor(0 , 15);
+    // dark blue board
+    setTextColor(0, 15);
     printf("7) \n");
-    setTextColor(9 , 15);
-    printBoard(Board,2,2);
+    setTextColor(9, 15);
+    printBoard(Board, 2, 2);
     printf("\n");
 
-    //gray board
-    setTextColor(0 , 15);
+    // gray board
+    setTextColor(0, 15);
     printf("8) \n");
-    setTextColor(8 , 15);
-    printBoard(Board,2,2);
+    setTextColor(8, 15);
+    printBoard(Board, 2, 2);
     printf("\n");
 
-    scanf("%d",&num);
-    if(num==1) return 0;
-    else if(num==2) return 6;
-    else if(num==3) return 13;
-    else if(num==4) return 12;
-    else if(num==5) return 11;
-    else if(num==6) return 10;
-    else if(num==7) return 9;
-    return 8;
+    // getting the input color and changing color
+    num = 0;
+    while (num <= 0 || num > 8)
+    {
+        scanf("%d", &num);
+        // checking if the input is valid
+        if (num == 1)
+            return 0;
+        else if (num == 2)
+            return 6;
+        else if (num == 3)
+            return 13;
+        else if (num == 4)
+            return 12;
+        else if (num == 5)
+            return 11;
+        else if (num == 6)
+            return 10;
+        else if (num == 7)
+            return 9;
+        else if (num == 8)
+            return 8;
+    }
 }
 
-# endif
+#endif
--- main.c
+++ main.c
@@ -5,54 +5,112 @@
 
 int main()
 {
-    setTextColor(0 , 15);
+    setTextColor(0, 15);
     clearScreen();
 
     // write welcome message
     printf("Welcome to Quoridor game!");
-    setTextColor(6 , 15);
-    printf(" %c\n",2);
+    setTextColor(6, 15);
+    printf(" %c\n", 2);
     sleep(2000);
     clearScreen();
 
-    // prints the arrow keys 
-    setTextColor(0 , 15);
+    // prints the arrow keys
+    setTextColor(0, 15);
     printf("You can move in the game using the arrow keys\n");
-    setTextColor(4 , 15);
+    setTextColor(4, 15);
     arrowKeyPrint();
     sleep(2000);
     clearScreen();
 
     // choosing a theme
     int color;
-    color=chooseBoard();
+    color = chooseBoard();
     clearScreen();
 
     // getting players' names
-    setTextColor(0 , 15);
+    setTextColor(0, 15);
     printf("Player(1) Please enter your name:\n");
-    scanf("%s",player1.name);
-    player1.nameInitial=player1.name[0];
+    scanf("%s", player1.name);
+    player1.nameInitial = player1.name[0];
     clearScreen();
 
     printf("Player(2) Please enter your name:\n");
-    scanf("%s",player2.name);
-    player2.nameInitial=player2.name[0];
+    scanf("%s", player2.name);
+    player2.nameInitial = player2.name[0];
     clearScreen();
 
     // getting row and column numbers
-    printf("Please enter the number of rows and columns:\n");
-    scanf("%d%d", &row, &column);
+    int rowIsNum = 0, colIsNum = 0;
+    while (!rowIsNum || !colIsNum)
+    {
+        printf("Please enter the number of rows and columns:\n");
+        char rowCopy[30], colCopy[30];
+        scanf("%s%s", rowCopy, colCopy);
+
+        int i;
+        for (i = 0; rowCopy[i] && '0' <= rowCopy[i] && rowCopy[i] <= '9'; i++)
+        {
+            row += rowCopy[i];
+            row *= 10;
+        }
+        row /= 10;
+        if (rowCopy[i] == 0)
+        {
+            rowIsNum = 1;
+        }
+
+        for (i = 0; colCopy[i] && '0' <= colCopy[i] && colCopy[i] <= '9'; i++)
+        {
+            column += colCopy[i];
+            column *= 10;
+        }
+        column /= 10;
+        if (colCopy[i] == 0)
+        {
+            colIsNum = 1;
+        }
+
+        if (!rowIsNum || !colIsNum)
+        {
+            printf("That wasn't a number!\n");
+        }
+    }
     clearScreen();
 
     // gettinng the number of walls
+    int wallIsNum = 0;
     int wallCount;
-    printf("Please enter the number of walls:\n");
-    scanf("%d",&wallCount);
-    player1.wallCount=player2.wallCount=wallCount;
+    while (!wallIsNum)
+    {
+        printf("Please enter the number of walls:\n");
+        char wallCountCopy[30];
+        scanf("%s", wallCountCopy);
+
+        int i;
+        for (i = 0; wallCountCopy[i] && '0' <= wallCountCopy[i] && wallCountCopy[i] <= '9'; i++)
+        {
+            wallCount += wallCountCopy[i];
+            wallCount *= 10;
+        }
+        wallCount /= 10;
+
+        if (wallCountCopy[i] == 0)
+        {
+            wallIsNum = 1;
+        }
+        if (!wallIsNum)
+        {
+            printf("That wasn't a number!\n");
+        }
+    }
     clearScreen();
 
-    // make the board 
+    // setting players' wall count:
+    player1.wallCount = player2.wallCount = wallCount;
+    
+
+    // make the board
     makeBoard(row, column);
     // makeSw();
 
@@ -62,7 +120,7 @@ int main()
         player1.location.x = 1;
         player1.location.y = column - 1;
 
-        player2.location.x = 2*row - 1;
+        player2.location.x = 2 * row - 1;
         player2.location.y = column + 1;
     }
     else
@@ -70,7 +128,7 @@ int main()
         player1.location.x = 1;
         player1.location.y = column;
 
-        player2.location.x = 2*row - 1;
+        player2.location.x = 2 * row - 1;
         player2.location.y = column;
     }
 
@@ -78,9 +136,8 @@ int main()
     Board[player1.location.x][player1.location.y] = player1.nameInitial;
     Board[player2.location.x][player2.location.y] = player2.nameInitial;
 
-
     // first print of the board:
-    setTextColor(color , 15);
+    setTextColor(color, 15);
     printBoard(Board, row, column);
 
     // integer round determines whose turn it is
@@ -88,7 +145,7 @@ int main()
     // if it is -1, it's player 2's turn
     int round = 1;
 
-    while (player1.location.x != 2*row - 1 && player2.location.x != 1) // while nobody has won
+    while (player1.location.x != 2 * row - 1 && player2.location.x != 1) // while nobody has won
     {
         getchar();
         if (round == 1)
@@ -97,21 +154,24 @@ int main()
             char moveChar = 0;
             while (moveChar != 'm' && moveChar != 'w')
             {
-                setTextColor(0 , 15);
-                printf("\nWall or move?\n(enter w for wall and m for move)\n");
+                setTextColor(0, 15);
+                printf("\nWall or move?\n(enter w for wall, and m for move)\n");
                 scanf("%c", &moveChar);
-                if (moveChar == 'W') moveChar = 'w';
-                else if (moveChar == 'M') moveChar = 'm';
+                if (moveChar == 'W')
+                    moveChar = 'w';
+                else if (moveChar == 'M')
+                    moveChar = 'm';
             }
 
             // placing a wall:
-            if (moveChar == 'w' && player1.wallCount != 0)  // runs only if the player still has walls left
+            if (moveChar == 'w' && player1.wallCount != 0) // runs only if the player still has walls left
             {
                 putWall();
-                player1.wallCount --;
+                player1.wallCount--;
                 clearScreen();
-                setTextColor(color , 15);
+                setTextColor(color, 15);
                 printBoard(Board, row, column);
+
                 // changing the round:
                 round *= -1;
             }
@@ -125,8 +185,9 @@ int main()
                 gotoxy(player1.location.x, player1.location.y);
                 move(&player1);
                 clearScreen();
-                setTextColor(color , 15);
+                setTextColor(color, 15);
                 printBoard(Board, row, column);
+
                 // changing the round:
                 round *= -1;
             }
@@ -137,19 +198,21 @@ int main()
             char moveChar = 0;
             while (moveChar != 'm' && moveChar != 'w')
             {
-                setTextColor(0 , 15);
-                printf("\nWall or move?\n(enter w for wall and m for move)\n");
+                setTextColor(0, 15);
+                printf("\nWall or move?\n(enter w for wall, and m for move)\n");
                 scanf("%c", &moveChar);
-                if (moveChar == 'W') moveChar = 'w';
-                else if (moveChar == 'M') moveChar = 'm';
+                if (moveChar == 'W')
+                    moveChar = 'w';
+                else if (moveChar == 'M')
+                    moveChar = 'm';
             }
             // placing a wall:
-            if (moveChar == 'w' && player2.wallCount != 0)  // runs only if the player still has walls left
+            if (moveChar == 'w' && player2.wallCount != 0) // runs only if the player still has walls left
             {
                 putWall();
                 clearScreen();
-                player2.wallCount --;
-                setTextColor(color , 15);
+                player2.wallCount--;
+                setTextColor(color, 15);
                 printBoard(Board, row, column);
                 // changing the round:
                 round *= -1;
@@ -164,26 +227,27 @@ int main()
                 gotoxy(player2.location.x, player2.location.y);
                 move(&player2);
                 clearScreen();
-                setTextColor(color , 15);
+                setTextColor(color, 15);
                 printBoard(Board, row, column);
+                
                 // changing the round:
                 round *= -1;
             }
         }
     }
-    
+
     // The previous loop has ended
     // hence someone has won
 
     // Determining the winner:
-    if (player1.location.x == 2*row - 1)
+    if (player1.location.x == 2 * row - 1)
     {
-        setTextColor(0 , 15);
+        setTextColor(0, 15);
         printf("%s wins!!!\n", player1.name);
     }
     else
     {
-        setTextColor(0 , 15);
+        setTextColor(0, 15);
         printf("%s wins!!!!\n", player2.name);
     }
     printf("Press enter to exit \n");
